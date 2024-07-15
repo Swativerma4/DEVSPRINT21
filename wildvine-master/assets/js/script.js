6f0cf041139a0992e6fa0f487b435462ec58207d
swati verma <swati2727verma@gmail.com>
2024-07-15 15:46:44 +0530

    commit

---
+++ wildvine-master/assets/js/script.js
@@ -0,0 +1,26 @@
+
+
+
+
+/**
+ * navbar toggle
+ */
+
+
+/**
+ * toggle navbar when click any navbar link
+ */
+
+
+
+
+
+/**
+ * header active when window scrolled down
+ */
+
+// JavaScript function to focus on the element with ID 'Rescue'
+
+document.getElementById("submit").addEventListener('click', function() {
+  window.location.href = 'page.html'; // Replace 'another-page.html' with the path to your target HTML page
+});
